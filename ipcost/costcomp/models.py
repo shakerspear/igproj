d6032cf838132d6dd75a5f43527af9d0587bab3a
shakerspear <yongtubuming@gmail.com>
2019-02-05 22:02:07 -0500

    start models

--- ipcost/costcomp/models.py
+++ ipcost/costcomp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+#counsolers
+class counsoler(models.Model):
+    name = models.CharField(max_length=64)
+    salary = models.IntegerField()
+    guaranteed = models.IntegerField()
+    total = models.IntegerField()
+#students
+
+#packages
